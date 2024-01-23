cbd67c13c3254c046f213a5dc7ac49bb4723c4b0
Allan Josue Chavez Sin <infodesarrollo12@minemgt.local>
2024-01-23 11:43:55 -0600

    feat: create-user dto and signin service function

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,22 +1,22 @@
 import { Controller, Post, Req, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Request } from 'express';
-import { AuthDto } from './dto';
+import { AuthDto, CreateUserDto } from './dto';
 
 @Controller('auth') // The decorator @Controller() takes a string argument that represents the path to the controller
 export class AuthController {
-    // In order to use a service from a controller, we need to inject, the way to do it is:
-    //1. By using the decorator @Injectable() in the service.
-    //2. In the controller we need to use a constructor and pass the service as an argument.
+  // In order to use a service from a controller, we need to inject, the way to do it is:
+  //1. By using the decorator @Injectable() in the service.
+  //2. In the controller we need to use a constructor and pass the service as an argument.
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup(@Body() dto: AuthDto) {
+  signup(@Body() dto: CreateUserDto) {
     return this.authService.signup(dto);
   }
 
   @Post('signin')
   signin(@Body() dto: AuthDto) {
-    return this.authService.login(dto);
+    return this.authService.signin(dto);
   }
 }
