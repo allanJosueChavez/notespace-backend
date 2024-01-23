ade5dbabff015193335ad4d0f5aa2b70651281c4
Allan Josue Chavez Sin <infodesarrollo12@minemgt.local>
2024-01-23 10:13:40 -0600

    feat: auth.controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,22 +1,22 @@
-import { Controller, Post , Req} from '@nestjs/common';
+import { Controller, Post, Req, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Request } from 'express';
- 
+import { AuthDto } from './dto';
 
-
-@Controller('auth')
-
+@Controller('auth') // The decorator @Controller() takes a string argument that represents the path to the controller
 export class AuthController {
-    constructor(private authService: AuthService) {}
+    // In order to use a service from a controller, we need to inject, the way to do it is:
+    //1. By using the decorator @Injectable() in the service.
+    //2. In the controller we need to use a constructor and pass the service as an argument.
+  constructor(private authService: AuthService) {}
 
-    @Post('signup')
-    signup(@Req() req : Request) {
-        return this.authService.signup()
-    }
+  @Post('signup')
+  signup(@Body() dto: AuthDto) {
+    return this.authService.signup(dto);
+  }
 
-    @Post('signin')
-    signin() {
-        return this.authService.login()
-    }
+  @Post('signin')
+  signin() {
+    return this.authService.login();
+  }
 }
-
