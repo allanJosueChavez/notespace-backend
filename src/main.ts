a3501198da789c4499dfccb65b04c0c6037ef510
Allan Josue Chavez Sin <infodesarrollo12@minemgt.local>
2024-01-23 10:14:42 -0600

    feat: install and use validationPipes globally by using them in the bootstrap function of the main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   // Bootstrap means to start the application. The word comes from the saying "to pull oneself up by one's bootstraps", which means to start from scratch.
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe(
+      // ValidationPipe is a pipe that uses class-validator to transform and validate payloads against a class model.
+      {
+        whitelist: true,
+        // forbidNonWhitelisted: true,
+        // transform: true,
+      },
+    ),
+  );
   await app.listen(3500);
 }
 bootstrap();
